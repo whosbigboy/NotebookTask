2284fb40f41a0714791e9b3c7aa607f28cd2a802
Baryshev Dmitry <dimoonbar0.5@gmail.com>
2024-06-10 23:24:34 +0400

    Второе начало

--- Commands.java
+++ Commands.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Commands {
+    public void Commands(){
+        String command = "";
+
+        while (command != "Закрыть"){
+            System.out.println("Введите команду\n");
+            Scanner sc = new Scanner(System.in);
+            System.out.println(sc);
+            switch(command){
+                case "Read":
+                    System.out.println("Read is right");
+                    break;
+                case "Write":
+                    System.out.println("Write is good");
+                    break;
+                default:
+                    System.out.println("Нет такой команды");
+                    break;
+            }
+        }
+    }
 }
